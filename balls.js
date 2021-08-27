cd439b45fe7859e65ade3c6eed5a749f9189709f
笑男 常 <1481167030@qq.com>
2021-08-27 23:24:37 +0800

    animation

---
+++ balls.js
@@ -0,0 +1,148 @@
+/**@type {HTMLCanvasElement} */
+
+var canvas=document.getElementById("canvas");
+var ctx=canvas.getContext("2d");
+function ball(x,y,r,vx,vy,color){
+    this.x=x,
+    this.y=y,
+    this.radius=r,
+    this.vx=vx,
+    this.vy=vy;
+    this.color=color,
+    this.run=function(){
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
+        ctx.closePath();
+        ctx.fillStyle=this.color;
+        ctx.fill();
+        this.x+=this.vx;
+        this.y+=this.vy;
+        if(this.x+this.radius>=canvas.width){
+            this.vx=-this.vx;
+            this.x=canvas.width-this.radius;
+        }
+        if(this.x-this.radius<=0){
+            this.vx=-this.vx;
+            this.x=this.radius;
+        }
+        if(this.y+this.radius>=canvas.height){
+            this.vy=-this.vy;
+            this.y=this.y-this.radius;
+        }
+        if(this.y-this.radius<=0){
+            this.vy=-this.vy;
+            this.y=this.radius;
+        }
+        
+    }
+};
+function clear(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+var balls=[];
+for(var i=0;i<100;i++){
+    var temp=new ball();
+    temp.x=Math.floor(Math.random()*canvas.width);
+    temp.y=Math.floor(Math.random()*canvas.height);
+    temp.radius=Math.floor(Math.random()*20);
+    temp.vx=Math.floor(Math.random()*20)-10;
+    temp.vy=Math.floor(Math.random()*20)-10;
+    temp.color="#"+Math.random().toString(16).substr(-6);
+    balls.push(temp);
+}
+
+function ballAnimation(){
+    requestAnimationFrame(ballAnimation);
+    clear();
+    for(var circle of balls){
+        circle.run();
+    }
+}
+
+var mouse={
+    x:null,
+    y:null,
+}
+
+window.onmousemove=function(e){
+    this.mouse.x=e.x;
+    this.mouse.y=e.y;
+}
+
+function point(x,y,vx,vy){
+    this.x=x;
+    this.y=y;
+    this.radius=0.5;
+    this.vx=vx;
+    this.vy=vy;
+    this.run=function(){
+        ctx.fillStyle="black";
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
+        ctx.closePath();
+        ctx.fill();
+        this.x+=this.vx;
+        this.y+=this.vy;
+        if(this.x+this.radius>=canvas.width){
+            this.vx=-this.vx;
+            this.x=canvas.width-this.radius;
+        }
+        if(this.x-this.radius<=0){
+            this.vx=-this.vx;
+            this.x=this.radius;
+        }
+        if(this.y+this.radius>=canvas.height){
+            this.vy=-this.vy;
+            this.y=this.y-this.radius;
+        }
+        if(this.y-this.radius<=0){
+            this.vy=-this.vy;
+            this.y=this.radius;
+        }
+        if(Math.pow(mouse.x-this.x,2)+Math.pow(mouse.y-this.y,2)<=Math.pow(100,2)){
+            ctx.strokeStyle="pink";
+            ctx.beginPath();
+            ctx.moveTo(mouse.x,mouse.y);
+            ctx.lineTo(this.x,this.y);
+            ctx.closePath();
+            ctx.stroke();
+        }
+    }
+}
+
+var points=[];
+var visit=[];
+for(var i=0;i<100;i++){
+    var x=Math.floor(Math.random()*canvas.width);
+    var y=Math.floor(Math.random()*canvas.height);
+    var vx=Math.random()>0.5?0.5:-0.5;
+    var vy=Math.random()>0.5?0.5:-0.5;
+    points.push(new point(x,y,vx,vy));
+    visit.push(false);
+}
+
+function pointAnimation(){
+    requestAnimationFrame(pointAnimation);
+    clear();
+    for(var i =0;i<100;i++){
+        points[i].run();
+        for(var j=0;j<100;j++){
+            if(j==i||visit[j]) continue;
+            if(Math.pow(points[i].x-points[j].y,2)+Math.pow(points[i].y-points[j].y,2)<=Math.pow(10,2)){
+                visit[j]=true;
+                ctx.strokeStyle="pink";
+                ctx.beginPath();
+                ctx.moveTo(points[i].x,points[i].y);
+                ctx.lineTo(points[j].x,points[j].y);
+                ctx.closePath();
+                ctx.stroke();
+            }
+            else{
+                visit[j]=false;
+            }
+        }
+    }
+}
+
+pointAnimation();
--- revolve.js
+++ revolve.js
@@ -1,24 +1,29 @@
 /**@type {HTMLCanvasElement} */
-var canvas=document.getElementById('can');
-var context=canvas.getContext("2d");
 
-function drawFiveAngle(canvasContent,x,y,radius){
+
+function drawFiveAngle(){
+    var canvas=document.getElementById('canvas');
+    var canvasContent=canvas.getContext("2d");
+    canvasContent.clearRect(0,0,800,600);
     var da=Math.PI/5*4;
-    //canvasContent.rotate(-Math.PI/3)
-
-    var color=canvasContent.createLinearGradient(x-radius,y-radius,x+radius,y+radius);
+    var x=300;
+    var y=300;
+    var radius=100;
+    canvasContent.save();
+    canvasContent.translate(x,y);
+    var color=canvasContent.createLinearGradient(-radius,-radius,radius,radius);
     color.addColorStop(0,"rgba(255,0,0,0.5)");
     color.addColorStop(0.5,"rgba(0,255,0,0.5)");
     color.addColorStop(1,"rgba(0,0,255,0.5)");
-
     canvasContent.fillStyle=color;
     canvasContent.strokeStyle='brown';
-    
+    var time = new Date();
+    canvasContent.rotate( ((2*Math.PI)/60)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds());
     canvasContent.beginPath();
     for(var i=0,angle=0;i<10;i++,angle+=da){
         var rx=radius*(Math.cos(angle));
         var ry=radius*(Math.sin(angle));
-        canvasContent.lineTo(rx+x,ry+y);
+        canvasContent.lineTo(rx,ry);
     }
     canvasContent.stroke();
     //阴影
@@ -28,30 +33,28 @@ function drawFiveAngle(canvasContent,x,y,radius){
     canvasContent.shadowColor="rgba(0,0,0,0.5)";
     canvasContent.fill();
     canvasContent.closePath();
-    canvasContent.clip();
+    //canvasContent.clip();
+    canvasContent.restore();
+    window.requestAnimationFrame(drawFiveAngle);
 }
-var type=["no-repeat","repeat","repeat-x","repeat-y"];
-var image=new Image();
-image.src="./file.ico";
-var i=0;
-image.onload=function(){
-    //context.drawImage(image,0,0,192*2,108*2);
-    // var pattern=context.createPattern(image,'no-repeat');
-    // context.fillStyle=pattern;
-    // context.fillRect(0,0,800,600);
-    var interval=setInterval(() => {
-        context.clearRect(0,0,800,600);
-        var pattern=context.createPattern(image,type[i++]);
-        if(i==4) i=0;
-        context.fillStyle=pattern;
-        context.fillRect(0,0,800,600);
-    }, 1000);
-}
-drawFiveAngle(context,300,300,100);
-//context.translate(300,300);
-var da=0;
-// var interval=setInterval(() => {
-//     drawFiveAngle(context,0,0,100);
-//     context.rotate(da++);
-    
-// }, 200);
+window.requestAnimationFrame(drawFiveAngle);
+
+// var type=["no-repeat","repeat","repeat-x","repeat-y"];
+// var image=new Image();
+// image.src="./file.ico";
+// var i=0;
+// image.onload=function(){
+//     //context.drawImage(image,0,0,192*2,108*2);
+//     // var pattern=context.createPattern(image,'no-repeat');
+//     // context.fillStyle=pattern;
+//     // context.fillRect(0,0,800,600);
+//     var interval=setInterval(() => {
+//         context.clearRect(0,0,800,600);
+//         var pattern=context.createPattern(image,type[i++]);
+//         if(i==4) i=0;
+//         context.fillStyle=pattern;
+//         context.fillRect(0,0,800,600);
+//     }, 1000);
+// }
+// drawFiveAngle(context,300,300,100);
+
